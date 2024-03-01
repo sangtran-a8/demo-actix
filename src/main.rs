fee8e395f3b9689a83e057e34d68a71227ee0312
sangtran-a8 <sangtran@ancient8.gg>
2024-03-01 10:08:32 +0700

    fix: deploy address

--- src/main.rs
+++ src/main.rs
@@ -46,7 +46,7 @@ async fn sum(Query(params): Query<AddParams>) -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(health).service(sum))
-        .bind("127.0.0.1:8080")?
+        .bind(("0.0.0.0", 8080))?
         .run()
         .await
 }
